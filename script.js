9969ed3281336cec4b6bc82751cadbd95377fbcd
Arash Kazerouni <arashkazerooni484@gmail.com>
2023-02-25 06:36:04 +0330

    hold button works

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ let scores, currentScore, activePlayer, playing;
 // initialize the game state
 activePlayer = 0;
 currentScore = 0;
+scores = [0, 0];
+playing = true;
+winningScore = 10;
 // function to switch between players
 function switchPlayer() {
   if (activePlayer === 0) {
@@ -32,15 +35,28 @@ function switchPlayer() {
   current0El.innerHTML = currentScore;
 }
 
-// Functions
-
+// Click on Roll Dice
 btnRoll.onclick = () => {
+  // Roll dice
   const randomNumber = Math.trunc(Math.floor(Math.random() * 6) + 1);
   diceEl.src = `http://127.0.0.1:5500/dice-${randomNumber}.png`;
+  // Add to Current Score
   randomNumber === 1 ? switchPlayer() : (currentScore += randomNumber);
-  if (activePlayer === 0) {
-    current0El.innerHTML = currentScore;
+  activePlayer === 0
+    ? (current0El.innerHTML = currentScore)
+    : (current1El.innerHTML = currentScore);
+};
+// Click on Hold Btn
+btnHold.onclick = () => {
+  const currentPlayer = document.querySelector(`.player--${activePlayer}`);
+  scores[activePlayer] += currentScore;
+  score0El.innerHTML = scores[0];
+  score1El.innerHTML = scores[1];
+
+  if (scores[activePlayer] < winningScore) {
   } else {
-    current1El.innerHTML = currentScore;
+    currentPlayer.classList.add('player--winner');
+    playing = false;
+    switchPlayer();
   }
 };
